01155278b913b73651d4f990198638b178dd73bf
Afolabi <opakunleafolabi@gmail.com>
2022-10-20 20:19:59 +0100

    user endpoint works

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const users = require('./routes/users')
+
 app.use(cors());
 app.use(bodyParser.json())
 
