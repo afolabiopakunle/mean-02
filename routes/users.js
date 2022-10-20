bc92801226889d59caf21cec08103c0b1122a3ca
Afolabi <opakunleafolabi@gmail.com>
2022-10-20 20:23:54 +0100

    implemented users endpoint

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,16 @@ router.get('/register', (req, res) => {
     res.send('Welcome users REGISTER')
 })
 
+router.get('/auth', (req, res) => {
+    res.send('Welcome users AUTH')
+})
+
+router.get('/profile', (req, res) => {
+    res.send('Welcome users PROFILE')
+})
+
+router.get('/validate', (req, res) => {
+    res.send('Welcome users VALIDATE')
+})
+
 module.exports = router;
